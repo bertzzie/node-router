0316c35b71e9c472a90398a37774fbb16be3682a
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-04-03 10:17:41 +0700

    initial commit

---
+++ NodeJSRouter/index.js
@@ -0,0 +1,16 @@
+﻿var ws = require('./WebServer'),
+    server = ws.WebServer({ 'port': 9000, 'hostname': 'localhost' });
+
+server.AddRoute("GET", /^\/$/, function (request, response, params) {
+    response.writeHead(200, { 'Content-Type': 'text/plain', 'Content-Length': 5 });
+    response.write('Hello');
+    response.end();
+});
+
+server.AddRoute("GET", /profile\/(.*)/, function (request, response, id) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write("PROFILE ID: " + id);
+    response.end();
+});
+
+server.Start();
